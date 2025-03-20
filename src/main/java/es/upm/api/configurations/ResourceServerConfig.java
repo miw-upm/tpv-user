c968ad3d7f6f86d4add5a1999e1c18f6db16ae91
JBernal <j.bernal@upm.es>
2025-03-20 11:29:57 +0100

    relative url with eureka

--- src/main/java/es/upm/api/configurations/ResourceServerConfig.java
+++ src/main/java/es/upm/api/configurations/ResourceServerConfig.java
@@ -1,6 +1,7 @@
 package es.upm.api.configurations;
 
 import es.upm.api.data.entities.Scope;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -12,6 +13,7 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.Collections;
 import java.util.Optional;
@@ -51,6 +53,12 @@ public class ResourceServerConfig {  // validate tokens y security APIs con SCOP
 
         });
         return jwtAuthenticationConverter;
+    }
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 
 }
