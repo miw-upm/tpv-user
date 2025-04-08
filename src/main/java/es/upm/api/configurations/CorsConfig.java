af04f6015095e79c15d42a13d76d6b137d11ac6e
JBernal <j.bernal@upm.es>
2025-04-08 11:29:22 +0200

    updated cors

--- src/main/java/es/upm/api/configurations/CorsConfig.java
+++ src/main/java/es/upm/api/configurations/CorsConfig.java
@@ -14,7 +14,12 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200", "https://jwt.io"));
+        configuration.setAllowedOrigins(List.of(
+                "http://localhost:4200",
+                "https://jwt.io",
+                "http://localhost:8082",
+                "http://localhost:8083"
+        ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true); // Asegúrate de que coincida con la política del Gateway
